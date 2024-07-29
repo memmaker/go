95b9a37323a27f2d54fbeeeefa05530ffc334aac
memmaker <memmaker@32kb.org>
2024-07-29 12:12:02 +0200

    add utils

--- util/files.go
+++ util/files.go
@@ -1,49 +1,65 @@
 package util
 
 import (
-	"bufio"
-	"io"
-	"os"
-	"path"
+    "bufio"
+    "io"
+    "os"
+    "path"
 )
 
+func MustLoad(open *os.File, err error) io.ReaderAt {
+    if err != nil {
+        panic(err)
+    }
+    return open
+}
+func MustOpen(filename string) io.ReadCloser {
+    open, _ := os.Open(filename)
+    return open
+}
+
+func FileExists(filename string) bool {
+    _, err := os.Stat(filename)
+    return !os.IsNotExist(err)
+}
+
 func ReadFileAsLines(filename string) []string {
-	var lines []string
-	file, err := os.Open(filename)
-	if err != nil {
-		return lines
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines
+    var lines []string
+    file, err := os.Open(filename)
+    if err != nil {
+        return lines
+    }
+    defer file.Close()
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+    return lines
 }
 
 func CreateFile(file string) io.WriteCloser {
-	f, err := os.Create(file)
-	if err != nil {
-		return nil
-	}
-	return f
+    f, err := os.Create(file)
+    if err != nil {
+        return nil
+    }
+    return f
 }
 
 func FilesInDirByExtension(dir string, extension string) []string {
-	files, err := os.ReadDir(dir)
-	if err != nil {
-		return nil
-	}
-	var result []string
-	for _, file := range files {
-		if file.IsDir() {
-			continue
-		}
-		if extension != "" && extension != file.Name()[len(file.Name())-len(extension):] {
-			continue
-		}
-		result = append(result, path.Join(dir, file.Name()))
-	}
-	return result
+    files, err := os.ReadDir(dir)
+    if err != nil {
+        return nil
+    }
+    var result []string
+    for _, file := range files {
+        if file.IsDir() {
+            continue
+        }
+        if extension != "" && extension != file.Name()[len(file.Name())-len(extension):] {
+            continue
+        }
+        result = append(result, path.Join(dir, file.Name()))
+    }
+    return result
 
 }
