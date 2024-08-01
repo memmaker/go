1d1453b47e1572500daec760a89dd2d29b034c49
memmaker <memmaker@32kb.org>
2024-08-01 22:20:29 +0200

    added itemcategories

--- fxtools/brushes.go
+++ fxtools/brushes.go
@@ -3,385 +3,381 @@ package fxtools
 import "github.com/memmaker/go/geometry"
 
 type Brush interface {
-    StartDrawing(pos geometry.Point)
-    DraggedOver(pos geometry.Point) []geometry.Point
-    StopDrawing(pos geometry.Point) []geometry.Point
-    Icon() rune
-    Name() string
+	StartDrawing(pos geometry.Point)
+	DraggedOver(pos geometry.Point) []geometry.Point
+	StopDrawing(pos geometry.Point) []geometry.Point
+	Icon() rune
+	Name() string
 }
 type PointSet map[geometry.Point]bool
 
 func NewPointSet() *PointSet {
-    set := PointSet{}
-    return &set
+	set := PointSet{}
+	return &set
 }
 
 func (s *PointSet) Add(pos geometry.Point) {
-    (*s)[pos] = true
+	(*s)[pos] = true
 }
 
 func (s *PointSet) ToSlice() []geometry.Point {
-    slice := make([]geometry.Point, 0, len(*s))
-    for pos := range *s {
-        slice = append(slice, pos)
-    }
-    return slice
+	slice := make([]geometry.Point, 0, len(*s))
+	for pos := range *s {
+		slice = append(slice, pos)
+	}
+	return slice
 }
 
 func (s *PointSet) Clear() {
-    clear(*s)
+	clear(*s)
 }
 
 func (s *PointSet) Cardinality() int {
-    return len(*s)
+	return len(*s)
 }
 
 func (s *PointSet) Pop() geometry.Point {
-    for pos := range *s {
-        delete(*s, pos)
-        return pos
-    }
-    return geometry.Point{}
+	for pos := range *s {
+		delete(*s, pos)
+		return pos
+	}
+	return geometry.Point{}
 }
 
 func (s *PointSet) Contains(neighbor geometry.Point) bool {
-    _, ok := (*s)[neighbor]
-    return ok
+	_, ok := (*s)[neighbor]
+	return ok
 }
 func NewPencil() *Pencil {
-    return &Pencil{
-        drawPositions: NewPointSet(),
-    }
+	return &Pencil{
+		drawPositions: NewPointSet(),
+	}
 }
 
 type Pencil struct {
-    drawPositions *PointSet
+	drawPositions *PointSet
 }
 
 func (p *Pencil) Name() string {
-    return "pencil"
+	return "pencil"
 }
 
 func (p *Pencil) StartDrawing(pos geometry.Point) {
-    p.drawPositions.Add(pos)
+	p.drawPositions.Add(pos)
 }
 
 func (p *Pencil) Icon() rune {
-    return 'p'
+	return 'p'
 }
 
 func (p *Pencil) DraggedOver(pos geometry.Point) []geometry.Point {
-    p.drawPositions.Add(pos)
-    return p.drawPositions.ToSlice()
+	p.drawPositions.Add(pos)
+	return p.drawPositions.ToSlice()
 }
 
 func (p *Pencil) StopDrawing(pos geometry.Point) []geometry.Point {
-    p.drawPositions.Add(pos)
-    slice := p.drawPositions.ToSlice()
-    p.drawPositions.Clear()
-    return slice
+	p.drawPositions.Add(pos)
+	slice := p.drawPositions.ToSlice()
+	p.drawPositions.Clear()
+	return slice
 }
 
 func NewLineBrush() *LineBrush {
-    return &LineBrush{}
+	return &LineBrush{}
 }
 
 type LineBrush struct {
-    startPos geometry.Point
+	startPos geometry.Point
 }
 
 func (p *LineBrush) Name() string {
-    return "line"
+	return "line"
 }
 
 func (p *LineBrush) StartDrawing(pos geometry.Point) {
-    p.startPos = pos
+	p.startPos = pos
 }
 
 func (p *LineBrush) Icon() rune {
-    return 'L'
+	return 'L'
 }
 
 func (p *LineBrush) DraggedOver(pos geometry.Point) []geometry.Point {
-    drawPositions := make([]geometry.Point, 0)
-    for _, linePos := range geometry.LineOfSight(p.startPos, pos, func(p geometry.Point) bool {
-        return true
-    }) {
-        drawPositions = append(drawPositions, linePos)
-    }
-    return drawPositions
+	return geometry.LineOfSight(p.startPos, pos, func(p geometry.Point) bool {
+		return true
+	})
 }
 
 func (p *LineBrush) StopDrawing(pos geometry.Point) []geometry.Point {
-    return p.DraggedOver(pos)
+	return p.DraggedOver(pos)
 }
 
 func NewOutlinedRectangleBrush() *RectangleBrush {
-    return &RectangleBrush{
-        drawRect: geometry.Rect{},
-        fill:     false,
-    }
+	return &RectangleBrush{
+		drawRect: geometry.Rect{},
+		fill:     false,
+	}
 }
 func NewFilledRectangleBrush() *RectangleBrush {
-    return &RectangleBrush{
-        drawRect: geometry.Rect{},
-        fill:     true,
-    }
+	return &RectangleBrush{
+		drawRect: geometry.Rect{},
+		fill:     true,
+	}
 }
 
 type RectangleBrush struct {
-    drawRect geometry.Rect
-    startPos geometry.Point
-    fill     bool
+	drawRect geometry.Rect
+	startPos geometry.Point
+	fill     bool
 }
 
 func (r *RectangleBrush) Name() string {
-    return "rect"
+	return "rect"
 }
 
 func (r *RectangleBrush) Icon() rune {
-    if r.fill {
-        return 'F'
-    }
-    return 'f'
+	if r.fill {
+		return 'F'
+	}
+	return 'f'
 }
 
 func (r *RectangleBrush) StartDrawing(pos geometry.Point) {
-    r.startPos = pos
-    r.drawRect = geometry.NewRect(pos.X, pos.Y, pos.X, pos.Y)
+	r.startPos = pos
+	r.drawRect = geometry.NewRect(pos.X, pos.Y, pos.X, pos.Y)
 }
 
 func (r *RectangleBrush) DraggedOver(pos geometry.Point) []geometry.Point {
-    r.drawRect = geometry.NewRect(r.startPos.X, r.startPos.Y, pos.X, pos.Y)
-    if r.fill {
-        return r.filledRect()
-    }
-    return r.outlinedRect()
+	r.drawRect = geometry.NewRect(r.startPos.X, r.startPos.Y, pos.X, pos.Y)
+	if r.fill {
+		return r.filledRect()
+	}
+	return r.outlinedRect()
 }
 
 func (r *RectangleBrush) StopDrawing(pos geometry.Point) []geometry.Point {
-    r.drawRect = geometry.NewRect(r.startPos.X, r.startPos.Y, pos.X, pos.Y)
-    if r.fill {
-        return r.filledRect()
-    }
-    return r.outlinedRect()
+	r.drawRect = geometry.NewRect(r.startPos.X, r.startPos.Y, pos.X, pos.Y)
+	if r.fill {
+		return r.filledRect()
+	}
+	return r.outlinedRect()
 }
 func (r *RectangleBrush) filledRect() []geometry.Point {
-    var filled []geometry.Point
-    for x := r.drawRect.Min.X; x <= r.drawRect.Max.X; x++ {
-        for y := r.drawRect.Min.Y; y <= r.drawRect.Max.Y; y++ {
-            filled = append(filled, geometry.Point{X: x, Y: y})
-        }
-    }
-    return filled
+	var filled []geometry.Point
+	for x := r.drawRect.Min.X; x <= r.drawRect.Max.X; x++ {
+		for y := r.drawRect.Min.Y; y <= r.drawRect.Max.Y; y++ {
+			filled = append(filled, geometry.Point{X: x, Y: y})
+		}
+	}
+	return filled
 }
 
 func (r *RectangleBrush) outlinedRect() []geometry.Point {
-    outine := NewPointSet()
-    for x := r.drawRect.Min.X; x <= r.drawRect.Max.X; x++ {
-        outine.Add(geometry.Point{X: x, Y: r.drawRect.Min.Y})
-        outine.Add(geometry.Point{X: x, Y: r.drawRect.Max.Y})
-    }
-    for y := r.drawRect.Min.Y; y <= r.drawRect.Max.Y; y++ {
-        outine.Add(geometry.Point{X: r.drawRect.Min.X, Y: y})
-        outine.Add(geometry.Point{X: r.drawRect.Max.X, Y: y})
-    }
-    return outine.ToSlice()
+	outline := NewPointSet()
+	for x := r.drawRect.Min.X; x <= r.drawRect.Max.X; x++ {
+		outline.Add(geometry.Point{X: x, Y: r.drawRect.Min.Y})
+		outline.Add(geometry.Point{X: x, Y: r.drawRect.Max.Y})
+	}
+	for y := r.drawRect.Min.Y; y <= r.drawRect.Max.Y; y++ {
+		outline.Add(geometry.Point{X: r.drawRect.Min.X, Y: y})
+		outline.Add(geometry.Point{X: r.drawRect.Max.X, Y: y})
+	}
+	return outline.ToSlice()
 }
 
 func NewOutlinedCircleBrush() *CircleBrush {
-    return &CircleBrush{
-        drawRect: geometry.Rect{},
-        fill:     false,
-        square:   true,
-    }
+	return &CircleBrush{
+		drawRect: geometry.Rect{},
+		fill:     false,
+		square:   true,
+	}
 }
 func NewFilledCircleBrush() *CircleBrush {
-    return &CircleBrush{
-        drawRect: geometry.Rect{},
-        fill:     true,
-        square:   true,
-    }
+	return &CircleBrush{
+		drawRect: geometry.Rect{},
+		fill:     true,
+		square:   true,
+	}
 }
 
 type CircleBrush struct {
-    drawRect geometry.Rect
-    startPos geometry.Point
-    fill     bool
-    square   bool
+	drawRect geometry.Rect
+	startPos geometry.Point
+	fill     bool
+	square   bool
 }
 
 func (r *CircleBrush) Name() string {
-    return "circle"
+	return "circle"
 }
 
 func (r *CircleBrush) Icon() rune {
-    if r.fill {
-        return 'C'
-    }
-    return 'c'
+	if r.fill {
+		return 'C'
+	}
+	return 'c'
 }
 
 func (r *CircleBrush) StartDrawing(pos geometry.Point) {
-    r.startPos = pos
-    r.drawRect = geometry.NewRect(pos.X, pos.Y, pos.X, pos.Y)
+	r.startPos = pos
+	r.drawRect = geometry.NewRect(pos.X, pos.Y, pos.X, pos.Y)
 }
 
 func (r *CircleBrush) DraggedOver(pos geometry.Point) []geometry.Point {
-    r.drawRect = geometry.NewRect(r.startPos.X, r.startPos.Y, pos.X, pos.Y)
-    if r.fill {
-        ellipse := r.outlinedEllipse()
-        if !ellipse.Contains(r.drawRect.Mid()) {
-            for _, fillPos := range floodFillFrom(r.drawRect.Mid(), func(src, from, to geometry.Point) bool {
-                return !ellipse.Contains(to)
-            }) {
-                ellipse.Add(fillPos)
-            }
-        }
-        return ellipse.ToSlice()
-    }
-    return r.outlinedEllipse().ToSlice()
+	r.drawRect = geometry.NewRect(r.startPos.X, r.startPos.Y, pos.X, pos.Y)
+	if r.fill {
+		ellipse := r.outlinedEllipse()
+		if !ellipse.Contains(r.drawRect.Mid()) {
+			for _, fillPos := range floodFillFrom(r.drawRect.Mid(), func(src, from, to geometry.Point) bool {
+				return !ellipse.Contains(to)
+			}) {
+				ellipse.Add(fillPos)
+			}
+		}
+		return ellipse.ToSlice()
+	}
+	return r.outlinedEllipse().ToSlice()
 }
 
 func (r *CircleBrush) StopDrawing(pos geometry.Point) []geometry.Point {
-    return r.DraggedOver(pos)
+	return r.DraggedOver(pos)
 }
 func (r *CircleBrush) filledCircle() []geometry.Point {
-    var filled []geometry.Point
-    // we want to draw a circle inside the bounds of the rectangle
-    shortestSide := IntMin(r.drawRect.Size().X, r.drawRect.Size().Y) - 1
-    radius := shortestSide / 2
-    for x := r.drawRect.Min.X + 1; x < r.drawRect.Max.X; x++ {
-        for y := r.drawRect.Min.Y + 1; y < r.drawRect.Max.Y; y++ {
-            if geometry.Distance(geometry.Point{X: x, Y: y}, r.drawRect.Mid())+0.5 < float64(radius) {
-                filled = append(filled, geometry.Point{X: x, Y: y})
-            }
-        }
-    }
-    return filled
+	var filled []geometry.Point
+	// we want to draw a circle inside the bounds of the rectangle
+	shortestSide := IntMin(r.drawRect.Size().X, r.drawRect.Size().Y) - 1
+	radius := shortestSide / 2
+	for x := r.drawRect.Min.X + 1; x < r.drawRect.Max.X; x++ {
+		for y := r.drawRect.Min.Y + 1; y < r.drawRect.Max.Y; y++ {
+			if geometry.Distance(geometry.Point{X: x, Y: y}, r.drawRect.Mid())+0.5 < float64(radius) {
+				filled = append(filled, geometry.Point{X: x, Y: y})
+			}
+		}
+	}
+	return filled
 }
 func (r *CircleBrush) outlinedEllipse() *PointSet {
-    result := NewPointSet()
-    x0 := r.drawRect.Min.X
-    y0 := r.drawRect.Min.Y
-    x1 := r.drawRect.Max.X
-    y1 := r.drawRect.Max.Y
-    a := geometry.Abs(x1 - x0)
-    b := geometry.Abs(y1 - y0)
-    b1 := b & 1 /* values of diameter */
-    dx := 4 * (1 - a) * b * b
-    dy := 4 * (b1 + 1) * a * a /* error increment */
+	result := NewPointSet()
+	x0 := r.drawRect.Min.X
+	y0 := r.drawRect.Min.Y
+	x1 := r.drawRect.Max.X
+	y1 := r.drawRect.Max.Y
+	a := geometry.Abs(x1 - x0)
+	b := geometry.Abs(y1 - y0)
+	b1 := b & 1 /* values of diameter */
+	dx := 4 * (1 - a) * b * b
+	dy := 4 * (b1 + 1) * a * a /* error increment */
 
-    err := dx + dy + b1*a*a /* error of 1.step */
-    e2 := 0                 /* error of 2.step */
+	err := dx + dy + b1*a*a /* error of 1.step */
+	e2 := 0                 /* error of 2.step */
 
-    if x0 > x1 {
-        x0 = x1
-        x1 += a
-    } /* if called with swapped points */
-    if y0 > y1 {
-        y0 = y1
-    } /* .. exchange them */
-    y0 += (b + 1) / 2
-    y1 = y0 - b1 /* starting pixel */
-    a *= 8 * a
-    b1 = 8 * b * b
+	if x0 > x1 {
+		x0 = x1
+		x1 += a
+	} /* if called with swapped points */
+	if y0 > y1 {
+		y0 = y1
+	} /* .. exchange them */
+	y0 += (b + 1) / 2
+	y1 = y0 - b1 /* starting pixel */
+	a *= 8 * a
+	b1 = 8 * b * b
 
-    for ok := true; ok; ok = x0 <= x1 {
-        result.Add(geometry.Point{X: x1, Y: y0}) /*   I. Quadrant */
-        result.Add(geometry.Point{X: x0, Y: y0}) /*  II. Quadrant */
-        result.Add(geometry.Point{X: x0, Y: y1}) /* III. Quadrant */
-        result.Add(geometry.Point{X: x1, Y: y1}) /*  IV. Quadrant */
-        e2 = 2 * err
-        if e2 <= dy {
-            y0++
-            y1--
-            dy += a
-            err += dy
-        } /* y step */
-        if e2 >= dx || 2*err > dy {
-            x0++
-            x1--
-            dx += b1
-            err += dx
-        } /* x step */
-    }
+	for ok := true; ok; ok = x0 <= x1 {
+		result.Add(geometry.Point{X: x1, Y: y0}) /*   I. Quadrant */
+		result.Add(geometry.Point{X: x0, Y: y0}) /*  II. Quadrant */
+		result.Add(geometry.Point{X: x0, Y: y1}) /* III. Quadrant */
+		result.Add(geometry.Point{X: x1, Y: y1}) /*  IV. Quadrant */
+		e2 = 2 * err
+		if e2 <= dy {
+			y0++
+			y1--
+			dy += a
+			err += dy
+		} /* y step */
+		if e2 >= dx || 2*err > dy {
+			x0++
+			x1--
+			dx += b1
+			err += dx
+		} /* x step */
+	}
 
-    for y0-y1 < b { /* too early stop of flat ellipses a=1 */
-        result.Add(geometry.Point{X: x0 - 1, Y: y0}) /* -> finish tip of ellipse */
-        result.Add(geometry.Point{X: x1 + 1, Y: y0})
-        y0++
-        result.Add(geometry.Point{X: x0 - 1, Y: y1})
-        result.Add(geometry.Point{X: x1 + 1, Y: y1})
-        y1--
-    }
-    return result
+	for y0-y1 < b { /* too early stop of flat ellipses a=1 */
+		result.Add(geometry.Point{X: x0 - 1, Y: y0}) /* -> finish tip of ellipse */
+		result.Add(geometry.Point{X: x1 + 1, Y: y0})
+		y0++
+		result.Add(geometry.Point{X: x0 - 1, Y: y1})
+		result.Add(geometry.Point{X: x1 + 1, Y: y1})
+		y1--
+	}
+	return result
 }
 func (r *CircleBrush) outlinedCircle() []geometry.Point {
-    var outline []geometry.Point
-    shortestSide := IntMin(r.drawRect.Size().X, r.drawRect.Size().Y) - 1
-    radius := shortestSide / 2
-    for x := r.drawRect.Min.X + 1; x < r.drawRect.Max.X; x++ {
-        for y := r.drawRect.Min.Y + 1; y < r.drawRect.Max.Y; y++ {
-            if int(geometry.Distance(geometry.Point{X: x, Y: y}, r.drawRect.Mid())+0.5) == radius {
-                outline = append(outline, geometry.Point{X: x, Y: y})
-            }
-        }
-    }
-    return outline
+	var outline []geometry.Point
+	shortestSide := IntMin(r.drawRect.Size().X, r.drawRect.Size().Y) - 1
+	radius := shortestSide / 2
+	for x := r.drawRect.Min.X + 1; x < r.drawRect.Max.X; x++ {
+		for y := r.drawRect.Min.Y + 1; y < r.drawRect.Max.Y; y++ {
+			if int(geometry.Distance(geometry.Point{X: x, Y: y}, r.drawRect.Mid())+0.5) == radius {
+				outline = append(outline, geometry.Point{X: x, Y: y})
+			}
+		}
+	}
+	return outline
 }
 
 func NewPaintBucket(traversable func(geometry.Point, geometry.Point, geometry.Point) bool) *PaintBucket {
-    return &PaintBucket{
-        traversable: traversable,
-    }
+	return &PaintBucket{
+		traversable: traversable,
+	}
 }
 
 type PaintBucket struct {
-    traversable func(geometry.Point, geometry.Point, geometry.Point) bool
+	traversable func(geometry.Point, geometry.Point, geometry.Point) bool
 }
 
 func (p *PaintBucket) Name() string {
-    return "fill"
+	return "fill"
 }
 
 func (p *PaintBucket) StartDrawing(pos geometry.Point) {}
 
 func (p *PaintBucket) Icon() rune {
-    return 'B'
+	return 'B'
 }
 
 func (p *PaintBucket) DraggedOver(pos geometry.Point) []geometry.Point {
-    return floodFillFrom(pos, p.traversable)
+	return []geometry.Point{pos}
 }
 
 func (p *PaintBucket) StopDrawing(pos geometry.Point) []geometry.Point {
-    return p.DraggedOver(pos)
+	return floodFillFrom(pos, p.traversable)
 }
 
 func IntMin(one int, two int) int {
-    if one < two {
-        return one
-    }
-    return two
+	if one < two {
+		return one
+	}
+	return two
 }
 
 func floodFillFrom(sourceOfFill geometry.Point, canTraverse func(geometry.Point, geometry.Point, geometry.Point) bool) []geometry.Point {
-    closedSet := NewPointSet()
-    openSet := NewPointSet()
-    openSet.Add(sourceOfFill)
-    neighbors := geometry.Neighbors{}
-    for openSet.Cardinality() > 0 {
-        current := openSet.Pop()
-        closedSet.Add(current)
-        for _, neighbor := range neighbors.Cardinal(current, func(potentialNeighbor geometry.Point) bool {
-            return canTraverse(sourceOfFill, current, potentialNeighbor)
-        }) {
-            if !closedSet.Contains(neighbor) {
-                openSet.Add(neighbor)
-            }
-        }
-    }
+	closedSet := NewPointSet()
+	openSet := NewPointSet()
+	openSet.Add(sourceOfFill)
+	neighbors := geometry.Neighbors{}
+	for openSet.Cardinality() > 0 {
+		current := openSet.Pop()
+		closedSet.Add(current)
+		for _, neighbor := range neighbors.Cardinal(current, func(potentialNeighbor geometry.Point) bool {
+			return canTraverse(sourceOfFill, current, potentialNeighbor)
+		}) {
+			if !closedSet.Contains(neighbor) {
+				openSet.Add(neighbor)
+			}
+		}
+	}
 
-    return closedSet.ToSlice()
+	return closedSet.ToSlice()
 }
--- textiles/itemCategory.go
+++ textiles/itemCategory.go
@@ -1,56 +1,73 @@
 package textiles
 
 import (
-    "github.com/memmaker/go/recfile"
-    "io"
-    "strconv"
+	"github.com/memmaker/go/recfile"
+	"image/color"
+	"io"
+	"strconv"
 )
 
 type ItemCategory struct {
-    Name string
-    Icon TextIcon
+	Name string
+	Icon TextIcon
 }
 
+func (c ItemCategory) WithForeground(rgba color.RGBA) ItemCategory {
+	c.Icon.Foreground = rgba
+	return c
+}
+func (c ItemCategory) WithBackground(rgba color.RGBA) ItemCategory {
+	c.Icon.Background = rgba
+	return c
+}
+func (c ItemCategory) WithIconChar(char rune) ItemCategory {
+	c.Icon.Char = char
+	return c
+}
+func (c ItemCategory) WithName(name string) ItemCategory {
+	c.Name = name
+	return c
+}
 func ReadItemCategoriesFile(reader io.Reader, palette ColorPalette) map[string]ItemCategory {
-    records := recfile.Read(reader)
-    categories := make(map[string]ItemCategory, len(records))
-    for _, record := range records {
-        category := recordToCategory(record, palette)
-        categories[category.Name] = category
-    }
-    return categories
+	records := recfile.Read(reader)
+	categories := make(map[string]ItemCategory, len(records))
+	for _, record := range records {
+		category := recordToCategory(record, palette)
+		categories[category.Name] = category
+	}
+	return categories
 }
 
 func recordToCategory(record recfile.Record, palette ColorPalette) ItemCategory {
-    category := ItemCategory{}
-    for _, field := range record {
-        switch field.Name {
-        case "Name":
-            category.Name = field.Value
-        case "Icon":
-            category.Icon.Char = []rune(field.Value)[0]
-        case "Foreground":
-            category.Icon.Foreground = palette.Get(field.Value)
-        case "Background":
-            category.Icon.Background = palette.Get(field.Value)
-        }
-    }
-    return category
+	category := ItemCategory{}
+	for _, field := range record {
+		switch field.Name {
+		case "Name":
+			category.Name = field.Value
+		case "Icon":
+			category.Icon.Char = []rune(field.Value)[0]
+		case "Foreground":
+			category.Icon.Foreground = palette.Get(field.Value)
+		case "Background":
+			category.Icon.Background = palette.Get(field.Value)
+		}
+	}
+	return category
 }
 
 func WriteItemCategoriesFile(writer io.StringWriter, categories map[string]ItemCategory, palette ColorPalette) error {
-    records := make([]recfile.Record, 0, len(categories))
-    for _, category := range categories {
-        records = append(records, categoryToRecord(category, palette))
-    }
-    return recfile.Write(writer, records)
+	records := make([]recfile.Record, 0, len(categories))
+	for _, category := range categories {
+		records = append(records, categoryToRecord(category, palette))
+	}
+	return recfile.Write(writer, records)
 }
 
 func categoryToRecord(category ItemCategory, palette ColorPalette) recfile.Record {
-    record := recfile.Record{}
-    record = append(record, recfile.Field{Name: "Name", Value: category.Name})
-    record = append(record, recfile.Field{Name: "Icon", Value: string(category.Icon.Char)})
-    record = append(record, recfile.Field{Name: "Foreground", Value: strconv.Itoa(palette.GetIndexOfColor(category.Icon.Foreground))})
-    record = append(record, recfile.Field{Name: "Background", Value: strconv.Itoa(palette.GetIndexOfColor(category.Icon.Background))})
-    return record
+	record := recfile.Record{}
+	record = append(record, recfile.Field{Name: "Name", Value: category.Name})
+	record = append(record, recfile.Field{Name: "Icon", Value: string(category.Icon.Char)})
+	record = append(record, recfile.Field{Name: "Foreground", Value: strconv.Itoa(palette.GetIndexOfColor(category.Icon.Foreground))})
+	record = append(record, recfile.Field{Name: "Background", Value: strconv.Itoa(palette.GetIndexOfColor(category.Icon.Background))})
+	return record
 }
