63c8be6aaa6dc4a89be4a9ca1254c0b13d4a98f7
memmaker <memmaker@32kb.org>
2024-08-05 20:43:38 +0200

    added changes

--- textiles/icons.go
+++ textiles/icons.go
@@ -1,6 +1,9 @@
 package textiles
 
-import "image/color"
+import (
+	"github.com/memmaker/go/foundation"
+	"image/color"
+)
 
 type TextIcon struct {
 	Char       rune
@@ -10,4 +13,12 @@ type TextIcon struct {
 
 func (t TextIcon) HasBackground() bool {
 	return t.Background.B == 255
+}
+
+func NewTextIconFromNamedColorChar(ncc foundation.NamedColorChar, palette ColorPalette) TextIcon {
+	return TextIcon{
+		Char:       ncc.Char,
+		Foreground: palette.Get(ncc.Fg),
+		Background: palette.Get(ncc.Bg),
+	}
 }
