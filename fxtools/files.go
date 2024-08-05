ee4695aa25b876b98804edad5315137b22da3884
memmaker <memmaker@32kb.org>
2024-08-05 23:06:19 +0200

    added changes

--- fxtools/files.go
+++ fxtools/files.go
@@ -1,68 +1,73 @@
 package fxtools
 
 import (
-    "bufio"
-    "io"
-    "os"
-    "path"
+	"bufio"
+	"io"
+	"os"
+	"path"
 )
 
 func MustLoad(open *os.File, err error) *os.File {
-    if err != nil {
-        panic(err)
-    }
-    return open
+	if err != nil {
+		panic(err)
+	}
+	return open
 }
 func MustOpen(filename string) *os.File {
-    return MustLoad(os.Open(filename))
+	return MustLoad(os.Open(filename))
 }
 
 func MustCreate(filename string) *os.File {
-    return MustLoad(os.Create(filename))
+	return MustLoad(os.Create(filename))
 }
 
 func FileExists(filename string) bool {
-    _, err := os.Stat(filename)
-    return !os.IsNotExist(err)
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
+}
+
+func DirExists(dir string) bool {
+	fi, err := os.Stat(dir)
+	return err == nil && fi.IsDir()
 }
 
 func ReadFileAsLines(filename string) []string {
-    var lines []string
-    file, err := os.Open(filename)
-    if err != nil {
-        return lines
-    }
-    defer file.Close()
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        lines = append(lines, scanner.Text())
-    }
-    return lines
+	var lines []string
+	file, err := os.Open(filename)
+	if err != nil {
+		return lines
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
 }
 
 func CreateFile(file string) io.WriteCloser {
-    f, err := os.Create(file)
-    if err != nil {
-        return nil
-    }
-    return f
+	f, err := os.Create(file)
+	if err != nil {
+		return nil
+	}
+	return f
 }
 
 func FilesInDirByExtension(dir string, extension string) []string {
-    files, err := os.ReadDir(dir)
-    if err != nil {
-        return nil
-    }
-    var result []string
-    for _, file := range files {
-        if file.IsDir() {
-            continue
-        }
-        if extension != "" && extension != file.Name()[len(file.Name())-len(extension):] {
-            continue
-        }
-        result = append(result, path.Join(dir, file.Name()))
-    }
-    return result
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil
+	}
+	var result []string
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		if extension != "" && extension != file.Name()[len(file.Name())-len(extension):] {
+			continue
+		}
+		result = append(result, path.Join(dir, file.Name()))
+	}
+	return result
 
 }
