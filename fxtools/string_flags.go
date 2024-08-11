1510a15ff3819fc7cccb0192396f37d493176673
memmaker <memmaker@32kb.org>
2024-08-11 12:15:35 +0200

    added changes

--- fxtools/string_flags.go
+++ fxtools/string_flags.go
@@ -5,34 +5,37 @@ import "strconv"
 type StringFlags map[string]int
 
 func (sf StringFlags) Get(key string) int {
-    if val, ok := sf[key]; ok {
-        return val
-    }
-    return 0
+	if val, ok := sf[key]; ok {
+		return val
+	}
+	return 0
 }
 
 func (sf StringFlags) Set(key string, val int) {
-    sf[key] = val
+	sf[key] = val
 }
 
 func (sf StringFlags) HasFlag(key string) bool {
-    return sf.Get(key) != 0
+	return sf.Get(key) != 0
 }
 
 func (sf StringFlags) SetFlag(key string) {
-    sf.Set(key, 1)
+	sf.Set(key, 1)
 }
 
 func (sf StringFlags) ClearFlag(key string) {
-    delete(sf, key)
+	delete(sf, key)
 }
 
 func (sf StringFlags) ToStringArray() []string {
-    var rows []TableRow
-    for key, val := range sf {
-        if val != 0 {
-            rows = append(rows, TableRow{Columns: []string{key, strconv.Itoa(val)}})
-        }
-    }
-    return TableLayout(rows, []TextAlignment{AlignLeft, AlignRight})
+	if len(sf) == 0 {
+		return []string{}
+	}
+	var rows []TableRow
+	for key, val := range sf {
+		if val != 0 {
+			rows = append(rows, TableRow{Columns: []string{key, strconv.Itoa(val)}})
+		}
+	}
+	return TableLayout(rows, []TextAlignment{AlignLeft, AlignRight})
 }
