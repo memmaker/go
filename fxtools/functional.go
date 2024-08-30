668b9388b589ab59e687bbf781d5b514db4effc8
memmaker <memmaker@32kb.org>
2024-08-30 14:33:19 +0200

    added changes

---
+++ fxtools/functional.go
@@ -0,0 +1,19 @@
+package fxtools
+
+func mapSlice[T1 any, T2 any](s []T1, f func(T1) T2) []T2 {
+	result := make([]T2, len(s))
+	for i, v := range s {
+		result[i] = f(v)
+	}
+	return result
+}
+
+func filterSlice[T any](s []T, f func(T) bool) []T {
+	result := make([]T, 0)
+	for _, v := range s {
+		if f(v) {
+			result = append(result, v)
+		}
+	}
+	return result
+}
