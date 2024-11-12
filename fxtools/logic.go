76cef4f6bc1b46aff7f5bbb4d41d3fb720300e0f
memmaker <4145035+memmaker@users.noreply.github.com>
2024-11-12 10:35:05 +0100

    adding and fixing

--- fxtools/logic.go
+++ fxtools/logic.go
@@ -1,10 +1,10 @@
 package fxtools
 
 import (
-	"fmt"
-	"regexp"
-	"strconv"
-	"strings"
+    "fmt"
+    "regexp"
+    "strconv"
+    "strings"
 )
 
 // pair with: https://github.com/Knetic/govaluate
@@ -14,133 +14,152 @@ import (
 type Arguments []string
 
 func (a Arguments) Get(index int) string {
-	if index >= len(a) || index < 0 {
-		return ""
-	}
-	return a[index]
+    if index >= len(a) || index < 0 {
+        return ""
+    }
+    return a[index]
+}
+
+func (a Arguments) GetBool(index int) bool {
+    if index >= len(a) || index < 0 {
+        return false
+    }
+    parseBool, _ := strconv.ParseBool(a[index])
+    return parseBool
 }
 
 func (a Arguments) GetInt(index int) int {
-	if index >= len(a) || index < 0 {
-		return 0
-	}
-	result, err := strconv.Atoi(a[index])
-	if err != nil {
-		return 0
-	}
-	return result
+    if index >= len(a) || index < 0 {
+        return 0
+    }
+    result, err := strconv.Atoi(a[index])
+    if err != nil {
+        return 0
+    }
+    return result
+}
+
+func (a Arguments) GetUint64(index int) uint64 {
+    if index >= len(a) || index < 0 {
+        return 0
+    }
+    result, err := strconv.ParseUint(a[index], 10, 64)
+    if err != nil {
+        return 0
+    }
+    return result
 }
 
 func (a Arguments) GetFloat(i int) float64 {
-	if i >= len(a) || i < 0 {
-		return 0
-	}
-	result, err := strconv.ParseFloat(a[i], 64)
-	if err != nil {
-		return 0
-	}
-	return result
+    if i >= len(a) || i < 0 {
+        return 0
+    }
+    result, err := strconv.ParseFloat(a[i], 64)
+    if err != nil {
+        return 0
+    }
+    return result
 }
 
 func (a Arguments) IsEmpty() bool {
-	return len(a) == 0
+    return len(a) == 0
 }
 
 var funcPattern = regexp.MustCompile(`([0-9A-Za-z_]+)\((.*)\)`)
 
 func LooksLikeAFunction(line string) bool {
-	return funcPattern.MatchString(line)
+    return funcPattern.MatchString(line)
 }
 func NewArgumentsFromString(args string) Arguments {
-	return trimAll(strings.Split(args, "|"))
+    return trimAll(strings.Split(args, "|"))
 }
 func GetNameAndArgs(line string) (string, Arguments) {
-	matches := funcPattern.FindStringSubmatch(line)
-	if matches == nil {
-		return line, Arguments{}
-	}
-	name := matches[1]
-	argString := matches[2]
-	if argString == "" {
-		return name, Arguments{}
-	}
-	args := trimAll(splitArgs(argString, ','))
-	return name, args
+    matches := funcPattern.FindStringSubmatch(line)
+    if matches == nil {
+        return line, Arguments{}
+    }
+    name := matches[1]
+    argString := matches[2]
+    if argString == "" {
+        return name, Arguments{}
+    }
+    args := trimAll(splitArgs(argString, ','))
+    return name, args
 }
 
 func splitArgs(argString string, delimiter rune) []string {
-	prevDelimiter := 0
-	var foundArgs []string
+    prevDelimiter := 0
+    var foundArgs []string
 
-	var insideString bool
-	var prevChar rune
-	var stringOpener rune
-	for i, r := range argString {
-		if prevChar != '\\' && ((!insideString && (r == '"' || r == '\'')) || (insideString && r == stringOpener)) {
-			insideString = !insideString
-			if insideString {
-				stringOpener = r
-			}
-		}
-		if r == delimiter && !insideString {
-			foundArgs = append(foundArgs, cleanArg(argString[prevDelimiter:i], stringOpener))
-			prevDelimiter = i + 1
-		}
-		prevChar = r
-	}
-	if prevDelimiter < len(argString) {
-		foundArgs = append(foundArgs, cleanArg(argString[prevDelimiter:], stringOpener))
-	}
-	return foundArgs
+    var insideString bool
+    var prevChar rune
+    var stringOpener rune
+    for i, r := range argString {
+        if prevChar != '\\' && ((!insideString && (r == '"' || r == '\'')) || (insideString && r == stringOpener)) {
+            insideString = !insideString
+            if insideString {
+                stringOpener = r
+            }
+        }
+        if r == delimiter && !insideString {
+            foundArgs = append(foundArgs, cleanArg(argString[prevDelimiter:i], stringOpener))
+            prevDelimiter = i + 1
+        }
+        prevChar = r
+    }
+    if prevDelimiter < len(argString) {
+        foundArgs = append(foundArgs, cleanArg(argString[prevDelimiter:], stringOpener))
+    }
+    return foundArgs
 }
 
 func cleanArg(arg string, quoteRune rune) string {
-	cutset := []rune(" \t")
-	if quoteRune == '"' {
-		cutset = append(cutset, '"')
-	} else if quoteRune == '\'' {
-		cutset = append(cutset, '\'')
-	}
-	return strings.ReplaceAll(strings.ReplaceAll(strings.Trim(arg, string(cutset)), "\\'", "'"), "\\\"", "\"")
+    cutset := []rune(" \t")
+    if quoteRune == '"' {
+        cutset = append(cutset, '"')
+    } else if quoteRune == '\'' {
+        cutset = append(cutset, '\'')
+    }
+    return strings.ReplaceAll(strings.ReplaceAll(strings.Trim(arg, string(cutset)), "\\'", "'"), "\\\"", "\"")
 }
 
 func trimAll(split []string) []string {
-	for i, s := range split {
-		split[i] = strings.TrimSpace(s)
-	}
-	return split
+    for i, s := range split {
+        split[i] = strings.TrimSpace(s)
+    }
+    return split
 }
 
 type CombinedPredicate struct {
-	andPredicates []func() bool
-	orPredicates  []func() bool
+    andPredicates []func() bool
+    orPredicates  []func() bool
 }
 
 func (p CombinedPredicate) And(predicates ...func() bool) CombinedPredicate {
-	p.andPredicates = append(p.andPredicates, predicates...)
-	return p
+    p.andPredicates = append(p.andPredicates, predicates...)
+    return p
 }
 func (p CombinedPredicate) Or(predicates ...func() bool) CombinedPredicate {
-	p.orPredicates = append(p.orPredicates, predicates...)
-	return p
+    p.orPredicates = append(p.orPredicates, predicates...)
+    return p
 }
 func (p CombinedPredicate) Evaluate() bool {
-	and := true
-	for _, predicate := range p.andPredicates {
-		and = and && predicate()
-	}
-	if len(p.orPredicates) == 0 {
-		return and
-	}
-	or := false
-	for _, predicate := range p.orPredicates {
-		or = or || predicate()
-	}
-	return and && or
+    and := true
+    for _, predicate := range p.andPredicates {
+        and = and && predicate()
+    }
+    if len(p.orPredicates) == 0 {
+        return and
+    }
+    or := false
+    for _, predicate := range p.orPredicates {
+        or = or || predicate()
+    }
+    return and && or
 }
 
 func (p CombinedPredicate) IsEmpty() bool {
-	return len(p.andPredicates) == 0 && len(p.orPredicates) == 0
+    return len(p.andPredicates) == 0 && len(p.orPredicates) == 0
 }
 
 type AnyFunc func(...any) any
@@ -149,125 +168,125 @@ type ActionFunc func(...any)
 type Predicate func(...any) bool
 
 type Logic struct {
-	PredicateMap map[string]Predicate
-	actionMap    map[string]ActionFunc
-	anyMap       map[string]AnyFunc
-	Variables    map[string]func() any
+    PredicateMap map[string]Predicate
+    actionMap    map[string]ActionFunc
+    anyMap       map[string]AnyFunc
+    Variables    map[string]func() any
 }
 
 func NewLogicCore() *Logic {
-	return &Logic{
-		PredicateMap: make(map[string]Predicate),
-		actionMap:    make(map[string]ActionFunc),
-		anyMap:       make(map[string]AnyFunc),
-		Variables:    make(map[string]func() any),
-	}
+    return &Logic{
+        PredicateMap: make(map[string]Predicate),
+        actionMap:    make(map[string]ActionFunc),
+        anyMap:       make(map[string]AnyFunc),
+        Variables:    make(map[string]func() any),
+    }
 }
 
 func (p *Logic) RegisterPredicate(name string, predicate Predicate) {
-	p.PredicateMap[name] = predicate
+    p.PredicateMap[name] = predicate
 }
 
 func (p *Logic) RegisterAction(name string, action ActionFunc) {
-	p.actionMap[name] = action
+    p.actionMap[name] = action
 }
 func (p *Logic) RegisterAny(name string, any AnyFunc) {
-	p.anyMap[name] = any
+    p.anyMap[name] = any
 }
 
 func (p *Logic) HandleAssignment(line string, resolveNow bool) {
-	parts := strings.Split(line, "=")
-	assignParts := trimAll(parts)
-	variableName := assignParts[0]
-	if LooksLikeAFunction(assignParts[1]) {
-		// variable is a function call
-		functionName, stringArgs := GetNameAndArgs(assignParts[1])
-		functionToCall := p.anyMap[functionName]
-		if resolveNow { // resolve now
-			args := p.StringResolve(stringArgs)
-			resolvedArgs := p.ResolveArgs(args)
-			if functionToCall == nil {
-				println(fmt.Sprintf("Script ERR: Function %s not found", functionName))
-				return
-			}
-			functionResult := functionToCall(resolvedArgs...)
-			p.Variables[variableName] = func() any {
-				return functionResult
-			}
-		} else { // lazy resolve when used
-			p.Variables[variableName] = func() any {
-				args := p.StringResolve(stringArgs)
-				functionResult := functionToCall(p.ResolveArgs(args)...)
-				return functionResult
-			}
-		}
-	} else {
-		// variable is a value
-		p.Variables[variableName] = func() any {
-			return assignParts[1]
-		}
-	}
+    parts := strings.Split(line, "=")
+    assignParts := trimAll(parts)
+    variableName := assignParts[0]
+    if LooksLikeAFunction(assignParts[1]) {
+        // variable is a function call
+        functionName, stringArgs := GetNameAndArgs(assignParts[1])
+        functionToCall := p.anyMap[functionName]
+        if resolveNow { // resolve now
+            args := p.StringResolve(stringArgs)
+            resolvedArgs := p.ResolveArgs(args)
+            if functionToCall == nil {
+                println(fmt.Sprintf("Script ERR: Function %s not found", functionName))
+                return
+            }
+            functionResult := functionToCall(resolvedArgs...)
+            p.Variables[variableName] = func() any {
+                return functionResult
+            }
+        } else { // lazy resolve when used
+            p.Variables[variableName] = func() any {
+                args := p.StringResolve(stringArgs)
+                functionResult := functionToCall(p.ResolveArgs(args)...)
+                return functionResult
+            }
+        }
+    } else {
+        // variable is a value
+        p.Variables[variableName] = func() any {
+            return assignParts[1]
+        }
+    }
 }
 func (p *Logic) LineToAction(line string) func() {
-	name, stringArgs := GetNameAndArgs(line)
-	println(fmt.Sprintf("Parsed action '%s(%s)'", name, stringArgs))
-	args := p.StringResolve(stringArgs)
-	action := p.actionMap[name]
-	if action == nil {
-		println(fmt.Sprintf("Script ERR: Action %s not found. Returning empty action.", name))
-		return func() {}
-	}
-	actionCall := func() {
-		action(p.ResolveArgs(args)...)
-	}
-	return actionCall
+    name, stringArgs := GetNameAndArgs(line)
+    println(fmt.Sprintf("Parsed action '%s(%s)'", name, stringArgs))
+    args := p.StringResolve(stringArgs)
+    action := p.actionMap[name]
+    if action == nil {
+        println(fmt.Sprintf("Script ERR: Action %s not found. Returning empty action.", name))
+        return func() {}
+    }
+    actionCall := func() {
+        action(p.ResolveArgs(args)...)
+    }
+    return actionCall
 }
 
 func (p *Logic) LineToPredicate(line string) func() bool {
-	name, stringArgs := GetNameAndArgs(line)
-	println(fmt.Sprintf("Parsed predicate '%s(%s)'", name, stringArgs))
-	args := p.StringResolve(stringArgs)
-	predicate := p.PredicateMap[name]
-	if predicate == nil {
-		println(fmt.Sprintf("Script ERR: Predicate %s not found. Returning always FALSE predicate.", name))
-		return func() bool { return false }
-	}
-	predicateCall := func() bool {
-		return predicate(p.ResolveArgs(args)...)
-	}
-	return predicateCall
+    name, stringArgs := GetNameAndArgs(line)
+    println(fmt.Sprintf("Parsed predicate '%s(%s)'", name, stringArgs))
+    args := p.StringResolve(stringArgs)
+    predicate := p.PredicateMap[name]
+    if predicate == nil {
+        println(fmt.Sprintf("Script ERR: Predicate %s not found. Returning always FALSE predicate.", name))
+        return func() bool { return false }
+    }
+    predicateCall := func() bool {
+        return predicate(p.ResolveArgs(args)...)
+    }
+    return predicateCall
 }
 
 func (p *Logic) ResolveArgs(args []func() any) []any {
-	resolvedArgs := make([]any, len(args))
-	for i, arg := range args {
-		if arg == nil {
-			println("Script ERR: Argument resolved to nil. Returning empty string.")
-			resolvedArgs[i] = ""
-		} else {
-			resolvedArgs[i] = arg()
-		}
-	}
-	return resolvedArgs
+    resolvedArgs := make([]any, len(args))
+    for i, arg := range args {
+        if arg == nil {
+            println("Script ERR: Argument resolved to nil. Returning empty string.")
+            resolvedArgs[i] = ""
+        } else {
+            resolvedArgs[i] = arg()
+        }
+    }
+    return resolvedArgs
 }
 func (p *Logic) StringResolve(stringArgs []string) []func() any {
-	args := make([]func() any, len(stringArgs))
-	for i, arg := range stringArgs {
-		value := arg
-		/*
-			if arg == "$PLAYER" {
-				args[i] = func() any { return p.Player }
-			} else
-		*/
-		if strings.HasPrefix(value, "$") {
-			args[i] = p.Variables[value]
-		} else {
-			args[i] = func() any { return value }
-		}
-	}
-	return args
+    args := make([]func() any, len(stringArgs))
+    for i, arg := range stringArgs {
+        value := arg
+        /*
+        	if arg == "$PLAYER" {
+        		args[i] = func() any { return p.Player }
+        	} else
+        */
+        if strings.HasPrefix(value, "$") {
+            args[i] = p.Variables[value]
+        } else {
+            args[i] = func() any { return value }
+        }
+    }
+    return args
 }
 
 func (p *Logic) ResolveVariable(name string) any {
-	return p.Variables[name]()
+    return p.Variables[name]()
 }
